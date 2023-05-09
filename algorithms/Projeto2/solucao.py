3539b8997caf10a9e58171059635d99b31e5cf69
marcinmss <marciomsantoss@gmail.com>
2023-05-09 07:09:33 -0300

    Removendo comentários desnecessários

--- algorithms/Projeto2/solucao.py
+++ algorithms/Projeto2/solucao.py
@@ -590,37 +590,6 @@ class Project_2(QgsProcessingAlgorithm):
 
 ##########################################################################################################################################        
         
-
-        # # Retrieve the feature source and sink. The 'dest_id' variable is used
-        # # to uniquely identify the feature sink, and must be included in the
-        # # dictionary returned by the processAlgorithm function.
-        # source = self.parameterAsSource(parameters, self.INPUT, context)
-        # (sink, dest_id) = self.parameterAsSink(parameters, self.OUTPUT,
-        #         context, source.fields(), source.wkbType(), source.sourceCrs())
-
-        # # Compute the number of steps to display within the progress bar and
-        # # get features from source
-        # total = 100.0 / source.featureCount() if source.featureCount() else 0
-        # features = source.getFeatures()
-
-        # for current, feature in enumerate(features):
-        #     # Stop the algorithm if cancel button has been clicked
-        #     if feedback.isCanceled():
-        #         break
-
-        #     # Add a feature in the sink
-        #     sink.addFeature(feature, QgsFeatureSink.FastInsert)
-
-        #     # Update the progress bar
-        #     feedback.setProgress(int(current * total))
-
-        # # Return the results of the algorithm. In this case our only result is
-        # # the feature sink which contains the processed features, but some
-        # # algorithms may return multiple feature sinks, calculated numeric
-        # # statistics, etc. These should all be included in the returned
-        # # dictionary, with keys matching the feature corresponding parameter
-        # # or output names.
-
         return {
             self.POINT_FLAGS: self.point_flag_id,
             self.LINE_FLAGS: self.line_flag_id,
